deb092da42900616f1586a3e6eb917d97b4827de
junayednoman <junayednoman05@gmail.com>
2023-11-06 09:03:46 +0600

    featured foods card design done

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,11 @@
 import { Helmet } from "react-helmet-async";
 import Banner from "./sections/banner/Banner";
+import { Avatar, Card, Tooltip } from "flowbite-react";
+import { CiLocationOn } from 'react-icons/ci';
+import { FcExpired } from 'react-icons/fc';
+import { FaUsers } from 'react-icons/fa';
+import { Link } from "react-router-dom";
+
 
 const Home = () => {
     return (
@@ -7,8 +13,119 @@ const Home = () => {
             <Helmet>
                 <title>Home | ShareABite - Waste Less, Feed</title>
             </Helmet>
-            
+
             <Banner></Banner>
+
+            {/* featured food */}
+            <div className="wrapper spacer">
+                <div className="text-center mb-10">
+                    <h3 className="font-semibold font-fontOswald text-[42px] section_title mb-5">Featured Foods</h3>
+                    <p>Discover Exceptional Donations to Satisfy Your Hunger</p>
+                </div>
+                <div>
+                    <Card
+                        className="max-w-sm shadow-none hover:shadow-md duration-200 food_card"
+                        imgAlt="Apple Watch Series 7 in colors pink, silver, and black"
+                        imgSrc="https://img.freepik.com/free-vector/fruits-frame-realistic-background_1284-29853.jpg?w=740&t=st=1699237504~exp=1699238104~hmac=e892849c04d454df24b31d9d772f846800fd08bfba9536440344bc32363c6d82"
+                    >
+                        <a href="#">
+                            <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
+                                Fresh Fruits
+                            </h5>
+                        </a>
+                        <div className="flex gap-2 justify-between items-center">
+                            <Avatar className="space-x-3 justify-start" img="https://www.flowbite-react.com/images/people/profile-picture-5.jpg" rounded>
+                                <div className="space-y-1 font-medium dark:text-white">
+                                    <Tooltip content='Donar' placement="right">
+                                    <div className="text-base font-semibold">Jese Leos</div>
+                                    </Tooltip>
+                                    <div className="flex gap-1 items-center">
+                                        <Tooltip placement="bottom" content='Pickup Location'>
+                                        <CiLocationOn></CiLocationOn>
+                                        </Tooltip>
+                                        <Tooltip placement="bottom" content='Pickup Location'>
+                                            <p className="text-sm">123 Elm Street</p>
+                                        </Tooltip>
+                                    </div>
+                                </div>
+                            </Avatar>
+                            <div className="justify-end">
+                                <div className="flex gap-1 items-center">
+                                    <FcExpired></FcExpired>
+                                    <Tooltip content='Expire Date'>
+                                        <p className="text-sm font-medium">2023-11-15</p>
+                                    </Tooltip>
+
+                                </div>
+                                <div className="flex gap-1 items-center">
+                                    <FaUsers></FaUsers>
+                                    <Tooltip content='Quantity' placement="bottom">
+                                        <p className="text-sm font-medium">12</p>
+                                    </Tooltip>
+
+                                </div>
+                            </div>
+                        </div>
+                        <div className="my-1">
+                            <p><span className="font-medium">Note: </span>Organically grown, assorted vegetables.</p>
+                        </div>
+                        {/* <div className="mb-5 mt-2.5 flex items-center">
+                            <svg
+                                className="h-5 w-5 text-yellow-300"
+                                fill="currentColor"
+                                viewBox="0 0 20 20"
+                                xmlns="http://www.w3.org/2000/svg"
+                            >
+                                <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                            </svg>
+                            <svg
+                                className="h-5 w-5 text-yellow-300"
+                                fill="currentColor"
+                                viewBox="0 0 20 20"
+                                xmlns="http://www.w3.org/2000/svg"
+                            >
+                                <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                            </svg>
+                            <svg
+                                className="h-5 w-5 text-yellow-300"
+                                fill="currentColor"
+                                viewBox="0 0 20 20"
+                                xmlns="http://www.w3.org/2000/svg"
+                            >
+                                <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                            </svg>
+                            <svg
+                                className="h-5 w-5 text-yellow-300"
+                                fill="currentColor"
+                                viewBox="0 0 20 20"
+                                xmlns="http://www.w3.org/2000/svg"
+                            >
+                                <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                            </svg>
+                            <svg
+                                className="h-5 w-5 text-yellow-300"
+                                fill="currentColor"
+                                viewBox="0 0 20 20"
+                                xmlns="http://www.w3.org/2000/svg"
+                            >
+                                <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                            </svg>
+                            <span className="ml-3 mr-2 rounded bg-cyan-100 px-2.5 py-0.5 text-xs font-semibold text-cyan-800 dark:bg-cyan-200 dark:text-cyan-800">
+                                5.0
+                            </span>
+                        </div> */}
+                        <div className="">
+
+                            <Link
+                                href="#"
+                                className="w-full block rounded-lg bg-[#9dc020e7] px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-[#9CC020] focus:outline-none focus:ring-4  dark:bg-cyan-600 dark:hover:bg-cyan-700 "
+                            >
+                                View Details
+                            </Link>
+                        </div>
+                    </Card>
+                </div>
+            </div>
 
         </div>
     );
